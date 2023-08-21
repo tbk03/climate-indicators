a08b3f0875d245b16917e02be5512dcdada97419
tbk03 <chrisjmartin03@gmail.com>
2023-08-21 17:07:20 +0100

    initial refactor of slope chart component

--- src/lib/Slope-Chart.svelte
+++ src/lib/Slope-Chart.svelte
@@ -1,16 +1,31 @@
 <script>
   export let year;
 
+  // ----------------------------------------------------------------------
+  // CHART DIMENSIONS
+  // ----------------------------------------------------------------------
+
   let chartWidth, innerWidth;
   let margin = { top: 30, right: 10, bottom: 100, left: 10 };
-  let comparisonOffset = 10;
-  
-  $: comparisonYear = year - comparisonOffset;
 
   $: {
     innerWidth = chartWidth - margin.right - margin.left;
-    console.log(innerWidth);
-    }
+  }
+
+  // ----------------------------------------------------------------------
+  // SCALES
+  // ----------------------------------------------------------------------
+  let xYear1, xYear2;
+
+  $:{
+    xYear1 = 0.2 * innerWidth;
+    xYear2 = 0.7 * innerWidth;
+  }
+  // ----------------------------------------------------------------------
+  // COMPARE THE TWO YEARS
+  // ----------------------------------------------------------------------
+  let comparisonOffset = 10;
+  $: comparisonYear = year - comparisonOffset;
 </script>
 
 <div class="waffle-title chart-title">
@@ -19,12 +34,24 @@
 <div class="slope-container chart-container">
   <div class="slope chart" bind:clientWidth={chartWidth}>
     <svg height="600" width={chartWidth}>
-        <g transform="translate({margin.left}, {margin.top})">
-            <text x={0.2 * innerWidth} y="0" dy="-10">{comparisonYear}</text>
-            <line x1={0.2 * innerWidth} x2={0.2*innerWidth} y1="0" y2="600" stroke="black"/>
-            <line x1={0.7 * innerWidth} x2={0.7*innerWidth} y1="0" y2="600" stroke="black"/>
-        </g>
-        
+      <g transform="translate({margin.left}, {margin.top})">
+        <text x={xYear1} y="0" dy="-10">{comparisonYear}</text>
+        <text x={xYear2} y="0" dy="-10">{year}</text>
+        <line
+          x1={xYear1}
+          x2={xYear1}
+          y1="0"
+          y2="600"
+          stroke="black"
+        />
+        <line
+          x1={xYear2}
+          x2={xYear2}
+          y1="0"
+          y2="600"
+          stroke="black"
+        />
+      </g>
     </svg>
   </div>
 </div>
