6948f148516beca624cee51dcf43e499ecca5fe3
tbk03 <chrisjmartin03@gmail.com>
2023-08-26 19:14:29 +0100

    set up scales and accessors

--- src/lib/LineChart.svelte
+++ src/lib/LineChart.svelte
@@ -1,19 +1,25 @@
 <script>
+  // D3
   import { csv } from "d3-fetch";
+  import { scaleLinear } from "d3-scale";
+  import { extent } from "d3-array";
+
+  // tidy-js
   import { tidy, mutate, filter } from "@tidyjs/tidy";
 
   // -----------------------------------------------------------------------------
   // IMPORT AND CLEAN DATA FROM
   // -----------------------------------------------------------------------------
 
+  let data = [];
+  loadData();
+
   // LOAD FROM GITHUB
   async function loadData() {
     const response = await csv(
       "https://raw.githubusercontent.com/ClimateIndicator/GHG-Emissions-Assessment/main/results/ghg_emissions_co2e.csv"
     );
-    const processed = await processData(response);
-    // console.log(processed);
-    return processed;
+    data = await processData(response);
   }
 
   // CLEAN DATA USING tidy.js
@@ -39,7 +45,47 @@
     return processed;
   }
 
-  let data = loadData();
+  // -----------------------------------------------------------------------------
+  // DIMENSIONS
+  // -----------------------------------------------------------------------------
+  let width = 400;
+  let height = 400;
+
+  // -----------------------------------------------------------------------------
+  // ACCESSORS
+  // -----------------------------------------------------------------------------
+  const xAccessor = (d) => d.year;
+  const yAccessor = (d) => d.total_ghg_emissions;
+  // -----------------------------------------------------------------------------
+  // SCALES
+  // -----------------------------------------------------------------------------
+  $: console.log(data);
+
+  $: xScale = scaleLinear()
+    .domain(extent(data, (d) => xAccessor(d)))
+    .range([0, width]);
+
+  $: yScale = scaleLinear()
+    // .domain(extent(data, (d) => yAccessor(d)))
+    .domain([0, 50])
+    .range([height, 0]);
 </script>
+
+{#await data}
+  <p>Loading...</p>
+{:then data}
+  <svg {width} {height}>
+    {#each data as d}
+      <circle
+        cx={xScale(xAccessor(d))}
+        cy={yScale(yAccessor(d))}
+        r={1}
+        fill="purple"
+        stroke="black"
+        stroke-width={1}
+      />
+    {/each}
+  </svg>
+{/await}
 
 <p>Line Chart</p>
