e89ac29cabf631ed0e3b75065188c4f86053148c
tbk03 <chrisjmartin03@gmail.com>
2023-08-27 21:04:44 +0100

    identify bug when hovering over chart area before it has loaded

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -1,6 +1,6 @@
 <script>
   import { tweened } from "svelte/motion";
-  import { cubicOut } from 'svelte/easing';
+  import { cubicOut } from "svelte/easing";
   export let positionOnChart;
   export let xScale;
   export let yScale;
@@ -8,28 +8,37 @@
   export let yAccessor;
   export let data;
 
-  let dataPoint = data.find((d) => xAccessor(d) === xAccessor(positionOnChart));
+  let dataPoint; 
+  dataPoint = data.find((d) => xAccessor(d) === xAccessor(positionOnChart));
+  //   console.log({ dataPoint });
 
   const tweenedTransition = {
     duration: 200,
-    easing: cubicOut
+    easing: cubicOut,
+  };
+
+  let nearestDataX;
+  let nearestDataY;
+  if (data) {
+    nearestDataX = tweened(xAccessor(dataPoint), tweenedTransition);
+    nearestDataY = tweened(yAccessor(dataPoint), tweenedTransition);
   }
-  let nearestDataX = tweened(xAccessor(dataPoint), tweenedTransition);
-  let nearestDataY = tweened(yAccessor(dataPoint), tweenedTransition);
 
   $: {
     dataPoint = data.find((d) => xAccessor(d) === xAccessor(positionOnChart));
     nearestDataX.set(xAccessor(dataPoint));
     nearestDataY.set(yAccessor(dataPoint));
   }
-  
+
   //   $: console.log(dataPoint);
   //   $: nearestDataX = tweened();
 </script>
 
-<circle
-  cx={xScale($nearestDataX)}
-  cy={yScale($nearestDataY)}
-  r={10}
-  fill="red"
-/>
+{#await data then data}
+  <circle
+    cx={xScale($nearestDataX)}
+    cy={yScale($nearestDataY)}
+    r={10}
+    fill="red"
+  />
+{/await}
