be0dcc7251533885c319b037f7e9ead5687fc5c0
tbk03 <chrisjmartin03@gmail.com>
2023-09-02 12:43:22 +0100

    make into an area chart

--- src/lib/components/Line.svelte
+++ src/lib/components/Line.svelte
@@ -4,32 +4,42 @@
   export let xAccessor;
   export let yAccessor;
   export let data;
-  export let color = "black";
+  export let color = "#C94A54";
   export let hoveredEvent;
 
   // D3.js
-  import { line, curveNatural } from "d3-shape";
+  import { line, area, curveNatural } from "d3-shape";
 
   // -----------------------------------------------------------------------------
   // LINE GENERATOR
   // -----------------------------------------------------------------------------
 
-  $: line_gen = line()
+  $: lineGen = line()
     .curve(curveNatural)
     // @ts-ignore
     .x((d) => xScale(xAccessor(d)))
     .y((d) => yScale(yAccessor(d)))(data);
+
+  $: areaGen = area()
+    .curve(curveNatural)
+    // @ts-ignore
+    .x((d) => xScale(xAccessor(d)))
+    .y0(yScale(0))
+    .y1((d) => yScale(yAccessor(d)))(data);
 </script>
 
+
+<path d={areaGen} fill="#EAC8CA" stroke="none" opacity="0.3"/>
 <path
   class="line"
-  d={line_gen}
+  d={lineGen}
   stroke={color}
-  opacity={hoveredEvent ? 0.5 : 1}
+  stroke-width={3}
+  opacity={hoveredEvent ? 0.2 : 1}
 />
 
 <style>
-  path {
+  .line {
     fill: transparent;
     transition-property: opacity;
     transition-duration: 1500ms;
